a5a5d0645d36a13ab2cf42573e11dfbd116f9eb0
Gopala Gabbita <gopala.gabbita@gmail.com>
2023-09-09 20:15:08 -0400

    updated

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Standard-Server" {
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   key_name                    = "ec2-tutorials"
-  ami                         = "ami-0f409bae3775dc8e5"
+  ami                         = "ami-0261755bbcb8c4a84"
   vpc_security_group_ids      = [aws_security_group.vpc_standard-sg.id]
   associate_public_ip_address = true
   availability_zone           = "us-east-1a"
