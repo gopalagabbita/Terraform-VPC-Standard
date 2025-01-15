0c4ce0ce1e18f75c386f2d4f060a8db84c4c2d34
gopalagabbita <gopala.gabbita@gmail.com>
2025-01-14 23:32:35 -0500

    terraform format

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "Standard-Server" {
-  instance_type               = "t2.medium"
+  instance_type               = "t2.micro"
   key_name                    = "ec2-tutorials"
   ami                         = "ami-0261755bbcb8c4a84"
   vpc_security_group_ids      = [aws_security_group.vpc_standard-sg.id]
